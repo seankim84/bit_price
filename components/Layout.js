4c23e7c5618e2614294094bb5648e632d1b40102
SeanKim <sean.kim84@gmail.com>
2018-10-18 12:01:24 +0700

    Connecting the Bootstrap

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ const Layout = props => (
     <div>
         <Head>
             <title>Currency Price</title>
+            <link href="https://bootswatch.com/4/cerulean/bootstrap.min.css" rel="stylesheet"/>
         </Head>
         <Navbar />
         {props.children}
